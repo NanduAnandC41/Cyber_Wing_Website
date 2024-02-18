2ccb7fa0711d13212f5fa8d9463ab6b801b38a1b
Anand C <anand.c@ibm.com>
2024-02-18 19:09:32 +0530

    cdr-cyber-bully-components-change

--- src/app/modules/cyber-bully-data/cyber-bully-data.component.ts
+++ src/app/modules/cyber-bully-data/cyber-bully-data.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavBarService } from '../../services/nav-bar.service';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule  } from "@angular/forms";
+
 
 @Component({
   selector: 'app-cyber-bully-data',
@@ -7,11 +9,49 @@ import { NavBarService } from '../../services/nav-bar.service';
   styleUrls: ['./cyber-bully-data.component.css']
 })
 export class CyberBullyDataComponent implements OnInit{
-
-  constructor(private navbarService: NavBarService) { }
+  public cyberBullyForm!: FormGroup;
+  constructor(private navbarService: NavBarService, protected formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    this.navbarService.setShowNavbar(false); 
+
+    this.navbarService.setShowNavbar(true);
+
+    this.cyberBullyForm = this.formBuilder.group({
+
+        fullName: new FormControl(),
+        sonDaughterWifeOf: new FormControl(),
+        dateOfBirth: new FormControl(),
+        fullAddress: new FormControl(),
+        aadharNo: new FormControl(),
+        mobileNo: new FormControl(),
+        emaiIdPersonal: new FormControl(),
+        emaiIdOfficial: new FormControl(),
+        professionJob: new FormControl(),
+        prevJobDetails: new FormControl(),
+        monthlyIncome: new FormControl(),
+        vehicleDetails: new FormControl(),
+        bankAccountDetails: new FormControl(),
+        panCardNo: new FormControl(),
+        drivingLicenseNo: new FormControl(),
+        passPortDetails: new FormControl(),
+        friendDetails: new FormControl(),
+        facebookDetails: new FormControl(),
+        twittedId: new FormControl(),
+        instagramId: new FormControl(),
+        youTubeDetails: new FormControl(),
+        whatsAppDetails: new FormControl(),
+        telegramNo: new FormControl(),
+        imoNo: new FormControl(),
+        tiktokId: new FormControl(),
+        heloId: new FormControl(),
+        linkedInId: new FormControl(),
+        previousCases: new FormControl(),
+        photoDetails: new FormControl(),
+
+
+
+      }
+    );
   }
 
 }
